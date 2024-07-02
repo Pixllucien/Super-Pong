d7b1a02e0c5ead13cc2be2745d0f83c295e36f48
Pixl_lucien <luciensmarsly@gmail.com>
2024-07-02 22:16:31 +0200

    Update

--- PongSpiel/src/game/Collision.java
+++ PongSpiel/src/game/Collision.java
@@ -34,10 +34,5 @@ public class Collision {
 			}
 	}
 	
-	public void collisionHorizontal(BeweglichesRechteck obj1, int screenheight){
-		if(obj1.positionY  >= screenheight) {
-			System.out.println("KOLLISION!!!!!!!!"); 
-			
-		}
-	}
+
 }
