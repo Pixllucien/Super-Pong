65100440b798f9c8aded56f33d1b19804e44c114
Klapprechner <Klapprechner@10.1.36.225>
2024-07-10 08:10:07 +0200

    Update #7

--- PongSpiel/src/game/GameLogic.java
+++ PongSpiel/src/game/GameLogic.java
@@ -6,51 +6,48 @@ import java.util.Timer;
 import java.util.TimerTask;
 import game.RandomBewegung;
 
-
 import gameObjects.BeweglichesRechteck;
 
 public class GameLogic {
-	
-	
+
 	private Timer gameTimer;
 	public int screenwidth;
 	public int screenheight;
 	public ArrayList<GameObject> spielObjekte;
-	
+
 	public boolean keyLeftarrowpressed;
 	public boolean keyRightarrowpressed;
-	public boolean keyUparrowpressed; 
-	public boolean keyDownarrowpressed; 
+	public boolean keyUparrowpressed;
+	public boolean keyDownarrowpressed;
 
 	public boolean keyWpressed;
 	public boolean keyApressed;
 	public boolean keySpressed;
 	public boolean keyDpressed;
-	public boolean runX = false; 
-	public boolean runY = false; 	
-	private int punkteSp1 = 0; 
-	private int punkteSp2 = 0;  
-	
-	private int add = 0; 
+	public boolean runX = false;
+	public boolean runY = false;
+	private int punkteSp1 = 0;
+	private int punkteSp2 = 0;
+	private int wait = 0;
+
+	private int add = 0;
 	Random rnd = new Random();
-	
+
 	private Collision objCollision;
-	public int screen; 
-	
+	public int screen;
+
 	public GameLogic() {
-		
-		objCollision = new Collision(); 
-	
+
+		objCollision = new Collision();
+
 		gameTimer = new Timer();
 		spielObjekte = new ArrayList<GameObject>();
-		RandomBewegung randomBewegung = new RandomBewegung(); 
+		RandomBewegung randomBewegung = new RandomBewegung();
 		keyLeftarrowpressed = false;
 		keyRightarrowpressed = false;
-		 
-	
-		
+
 		// Objekte im Spiel:
-		
+
 		BeweglichesRechteck ball = new BeweglichesRechteck(350, 280, 20, 20);
 		spielObjekte.add(ball);
 		ball.richtung = 0; // Startrichtung
@@ -58,54 +55,48 @@ public class GameLogic {
 		BeweglichesRechteck spieler2 = new BeweglichesRechteck(735, 280, 20, 80);
 		spielObjekte.add(spieler1);
 		spielObjekte.add(spieler2);
-		gameTimer.scheduleAtFixedRate(new TimerTask(){
+		gameTimer.scheduleAtFixedRate(new TimerTask() {
 			@Override
 			public void run() {
 
-				if(keyUparrowpressed) {
-					spieler2.positionY -= 1; 
+				if (keyUparrowpressed) {
+					spieler2.positionY -= 1;
+				} else if (keyDownarrowpressed) {
+					spieler2.positionY += 1;
 				}
-				else if(keyDownarrowpressed) {
-					spieler2.positionY +=1; 
-				}
-				
-				
-			    if(keyWpressed) {
-					spieler1.positionY -= 1; 
-				}
-		     	else if(keySpressed) {
-					spieler1.positionY +=1; 
-				}
-				
 
-				
+				if (keyWpressed) {
+					spieler1.positionY -= 1;
+				} else if (keySpressed) {
+					spieler1.positionY += 1;
+				}
 
-			randomBewegung.randomBewegungX(ball, runX, screenheight, spieler1, spieler2);
-			runX = true; 	
-			randomBewegung.randomBewegungY(ball, runY, screenheight, spieler1, spieler2);
-			runY = true; 
-			setPunkteSp2(objCollision.collisonLeft(ball, 0)); 
-			setPunkteSp1(objCollision.collisionRight(ball, 760)); 
-			System.out.println("Spielstand: " + getPunkteSp1() + "   " + getPunkteSp2());
-			
-			if(objCollision.collisionRight(ball, 760) == 1 || objCollision.collisonLeft(ball, 0) == 1) {
-				ball.positionX = 350; 
-				ball.positionY = 280; 
-				runX = false;
-				runY = false;	
-				//hier bitte warten
+				if (wait <= 0) {
+					randomBewegung.randomBewegungX(ball, runX, screenheight, spieler1, spieler2);
+					runX = true;
+					randomBewegung.randomBewegungY(ball, runY, screenheight, spieler1, spieler2);
+					runY = true;
+				}
+				
+				setPunkteSp2(objCollision.collisonLeft(ball, 0));
+				setPunkteSp1(objCollision.collisionRight(ball, 760));
+				System.out.println("Spielstand: " + getPunkteSp1() + "   " + getPunkteSp2());
+
+				if (objCollision.collisionRight(ball, 760) == 1 || objCollision.collisonLeft(ball, 0) == 1) {
+					ball.positionX = 350;
+					ball.positionY = 280;
+					runX = false;
+					runY = false;
+					// hier bitte warten
+					wait = 100;
+				}
+				if (wait > 0) {
+					wait--;
+				}
+
 			}
-
-			
-			}
-		}, 0, 5);  // war mal 5 
+		}, 0, 5); // war mal 5
 	}
-	
-	
-	
-	
-	
-	
 
 	public int getPunkteSp2() {
 		return punkteSp2;
@@ -123,5 +114,4 @@ public class GameLogic {
 		this.punkteSp1 += punkteSp1;
 	}
 
-	
 }
